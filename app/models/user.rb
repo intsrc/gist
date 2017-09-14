1856445d842444c7886bacad33a408103f3f7f49
Volodymyr <dev.volodymyr@gmail.com>
2017-09-15 01:16:20 +0300

    Implemented sessions (login&logout functionality)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :snippets
 
+  validates_length_of :username, minimum: 3, maximum: 16
+  validates :email, email: true, uniqueness: true
+
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   root 'pages#index'
   resources :users
+  resources :gitst, only: ['new', 'create', 'show']
 end
