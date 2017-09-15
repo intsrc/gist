24e59b5aa32522b8cdec36fe33ac221319137136
Volodymyr <dev.volodymyr@gmail.com>
2017-09-15 12:09:16 +0300

    Added validation rules

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :snippet
 
+  validates :content, length: { minimum: 3, maximum: 20000 }
+  validates_numericality_of :user_id,    only_integer: true
+  validates_numericality_of :snippet_id, only_integer: true
+
   paginates_per 10
 end
--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -2,5 +2,8 @@ class Snippet < ApplicationRecord
   belongs_to :user
   has_many   :comments
 
+  validates :content, length: { minimum: 3, maximum: 20000 }
+  validates_numericality_of :user_id, only_integer: true
+
   paginates_per 10
 end
