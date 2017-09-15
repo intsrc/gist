11728d96914556316e2ab5aa5e7c0db89d40e11a
Volodymyr <dev.volodymyr@gmail.com>
2017-09-15 10:20:52 +0300

    Removed auto-generated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-
-  get 'comments/edit'
-
-  get 'comments/update'
-
-  get 'comments/destroy'
-
   get    '/login',  to: 'sessions#new'
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
